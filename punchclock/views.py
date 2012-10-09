e9bfd638ac20353e979ab21d6941a5779353e18d
mtarsel <mtarsel@gmail.com>
2012-10-09 13:53:14 -0400

    Index.html now displays basic idea of login page. Department only present if user registers with department

--- punchclock/views.py
+++ punchclock/views.py
@@ -1,11 +1,10 @@
 from django.template import Context, loader
 from punchclock.models import User
-from punchclock.models import ClockEvent
 #from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def index(request):
-    user_list = User.objects.all().order_by('-last_name')[:5]
-    return render_to_response('punchclock/index.html', {'user_list': user_list})
+    department_list = User.objects.all().order_by('-department')[:5]
+    return render_to_response('punchclock/index.html', {'department_list': department_list})
     
     
