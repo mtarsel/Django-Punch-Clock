e63cbaecf196e7eeaf993cc67e6f30857dc2ee57
mtarsel <mtarsel@gmail.com>
2013-01-23 16:35:57 -0500

    Created TimeCard_Management to fix bugs. Error-checking in place

--- punchclock/models.py
+++ punchclock/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from datetime import datetime
-from django.utils import timezone
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
 from django.forms import ModelForm
 from django.shortcuts import render_to_response
-from datetime import date,time
+from datetime import *
 
 class Account(models.Model):
 	name = models.CharField(max_length=512)
@@ -32,10 +30,8 @@ class User(models.Model):
 	amount_paid = models.FloatField()
 	department = models.ForeignKey(Department)#user can have multiple departments
 	account = models.ForeignKey(Account)#can have multiple accounts
-	
 	in_time = models.DateField(null = True, blank = True)
 	is_in = models.BooleanField(False)
-	
 	out_time = models.DateField(null = True)
 	is_out = models.BooleanField(False)
 	#if they work for one year, increase pay by $0.25
@@ -44,99 +40,97 @@ class User(models.Model):
 		return unicode(self.last_name) + ', ' + unicode(self.first_name) + ':' + unicode(self.number) + ' $' + unicode(self.pay_rate)
 		
 
-class Timecard(models.Model):
-	user = models.ForeignKey(User)
-	today = datetime.today()
+#class Timecard(models.Model):
+	#user = models.ForeignKey(User)
+	#today = datetime.today()
 	
-	def generate_timecards(self):
-	# Create the HttpResponse object with the appropriate PDF headers.
-		response = HttpResponse(mimetype='application/pdf')
-		response['Content-Disposition'] = 'attachment; filename="timecards.pdf"'
+	#def generate_timecards(self):
+	## Create the HttpResponse object with the appropriate PDF headers.
+		#response = HttpResponse(mimetype='application/pdf')
+		#response['Content-Disposition'] = 'attachment; filename="timecards.pdf"'
 
-		buffer = BytesIO()
-		# Create the PDF object, using the BytesIO object as its "file."
-		p = canvas.Canvas(buffer)
+		#buffer = BytesIO()
+		## Create the PDF object, using the BytesIO object as its "file."
+		#p = canvas.Canvas(buffer)
 	
-		p.setLineWidth(.3)
-		p.setFont('Courier-Bold', 8.5)
+		#p.setLineWidth(.3)
+		#p.setFont('Courier-Bold', 8.5)
  
-		p.drawString(215,778, 'Budgets charged')
-		p.drawString(215, 762, '%s') %(user.department)#make Account number!!
-		p.drawString(360, 778, 'Hours')
-		p.drawString(425, 778, 'Rate')
-		p.grid([210, 355, 420, 460], [730, 745, 760, 775, 790])
+		#p.drawString(215,778, 'Budgets charged')
+		#p.drawString(215, 762, '%s') %(user.department)#make Account number!!
+		#p.drawString(360, 778, 'Hours')
+		#p.drawString(425, 778, 'Rate')
+		#p.grid([210, 355, 420, 460], [730, 745, 760, 775, 790])
 
-		#x, y, width, height
-		p.rect(25, 740, 175, 60, stroke=1, fill=0)
-		p.drawString(55,775,'EMPLOYEE TIME REPORT')
-		p.drawString(55,762,'For Clarkson Students')
-		p.drawString(95,750, 'use only')
+		##x, y, width, height
+		#p.rect(25, 740, 175, 60, stroke=1, fill=0)
+		#p.drawString(55,775,'EMPLOYEE TIME REPORT')
+		#p.drawString(55,762,'For Clarkson Students')
+		#p.drawString(95,750, 'use only')
 
-		p.drawString(500,783, '%s') %(user.number)#make this the student numbers
-		p.line(480,780,580,780)
-		p.drawString(500,773,'Student No.')#770-755
+		#p.drawString(500,783, '%s') %(user.number)#make this the student numbers
+		#p.line(480,780,580,780)
+		#p.drawString(500,773,'Student No.')#770-755
 
-		p.drawString(500, 753, '%s') %(today) #must be date
-		p.line(480,750,580,750)
-		p.drawString(480, 738, 'Month   Day   Year')
+		#p.drawString(500, 753, '%s') %(today) #must be date
+		#p.line(480,750,580,750)
+		#p.drawString(480, 738, 'Month   Day   Year')
  
-		p.drawString(112, 708, '%s %%s') %(user.first_name) %(user.last_name)#user.firstname, user.lastname
-		p.drawString(30,705,'Print name:')
-		p.line(92,705,250,705)
+		#p.drawString(112, 708, '%s %%s') %(user.first_name) %(user.last_name)#user.firstname, user.lastname
+		#p.drawString(30,705,'Print name:')
+		#p.line(92,705,250,705)
 	
-		p.drawString(395, 708, '10')#TOTAL HOURS!!!
-		p.drawString(255,705,'Total Hours Worked:')
-		p.line(375,705,490,705)
+		#p.drawString(395, 708, '10')#TOTAL HOURS!!!
+		#p.drawString(255,705,'Total Hours Worked:')
+		#p.line(375,705,490,705)
 		
-		#FOR LOOP to start the dates properly.
+		##FOR LOOP to start the dates properly.
 		
-		#Parse dates from Clockin and ClockOut to represent just the dates!!!
+		##Parse dates from Clockin and ClockOut to represent just the dates!!!
 	
-		p.grid([25, 60, 95, 130, 165, 200, 235, 270, 305, 340, 375, 410, 445], [655, 670, 685, 700])
-		p.drawString(33, 658, 'Sun')
-		p.drawString(33, 672, '1.5')
-		p.drawString(33,688, '11/2')
+		#p.grid([25, 60, 95, 130, 165, 200, 235, 270, 305, 340, 375, 410, 445], [655, 670, 685, 700])
+		#p.drawString(33, 658, 'Sun')
+		#p.drawString(33, 672, '1.5')
+		#p.drawString(33,688, '11/2')
 	
-		p.drawString(68, 658, 'Mon')
-		p.drawString(68, 672, '0')
-		p.drawString(68,688, '11/3')
+		#p.drawString(68, 658, 'Mon')
+		#p.drawString(68, 672, '0')
+		#p.drawString(68,688, '11/3')
 	
-		p.drawString(100, 658, 'Tues')
-		p.drawString(135, 658, 'Wed')
-		p.drawString(166, 658, 'Thurs')
-		p.drawString(208, 658, 'Fri')
-		p.drawString(243, 658, 'Sat')
-		p.drawString(278, 658, 'Sun')
-		p.drawString(313, 658, 'Mon')
-		p.drawString(345, 658, 'Tues')
-		p.drawString(380, 658, 'Wed')
-		p.drawString(413, 658, 'Thurs')
+		#p.drawString(100, 658, 'Tues')
+		#p.drawString(135, 658, 'Wed')
+		#p.drawString(166, 658, 'Thurs')
+		#p.drawString(208, 658, 'Fri')
+		#p.drawString(243, 658, 'Sat')
+		#p.drawString(278, 658, 'Sun')
+		#p.drawString(313, 658, 'Mon')
+		#p.drawString(345, 658, 'Tues')
+		#p.drawString(380, 658, 'Wed')
+		#p.drawString(413, 658, 'Thurs')
 	
-		p.grid([470, 505, 540], [655, 670, 685, 700])
-		p.drawString(478,658, 'Fri')
-		p.drawString(513,658, 'Sat')
+		#p.grid([470, 505, 540], [655, 670, 685, 700])
+		#p.drawString(478,658, 'Fri')
+		#p.drawString(513,658, 'Sat')
 	
-		p.rect(60, 595, 505, 40, stroke=1, fill=0)
-		p.grid([60, 305, 565 ], [595, 618])
-		p.drawString(125, 623, 'Work was performed in a satisfactory manner. IAW CFR 175.19.')
-		p.drawString(65, 598, 'Student Signature')
-		p.drawString(310, 598, 'Dept. Head Signature')
+		#p.rect(60, 595, 505, 40, stroke=1, fill=0)
+		#p.grid([60, 305, 565 ], [595, 618])
+		#p.drawString(125, 623, 'Work was performed in a satisfactory manner. IAW CFR 175.19.')
+		#p.drawString(65, 598, 'Student Signature')
+		#p.drawString(310, 598, 'Dept. Head Signature')
 		
-		p.rect(5, 590, 580, 220, stroke=1, fill=0)
+		#p.rect(5, 590, 580, 220, stroke=1, fill=0)
 	
-		p.drawString(100, 100, "Hello world.")
+		#p.drawString(100, 100, "Hello world.")
 
-		# Close the PDF object cleanly.
-		p.showPage()
-		p.save()
+		## Close the PDF object cleanly.
+		#p.showPage()
+		#p.save()
 
-		# Get the value of the BytesIO buffer and write it to the response.
-		pdf = buffer.getvalue()
-		buffer.close()
-		response.write(pdf)
-		return response
-
-#class Report(models.Model):
+		## Get the value of the BytesIO buffer and write it to the response.
+		#pdf = buffer.getvalue()
+		#buffer.close()
+		#response.write(pdf)
+		#return response
 
 class ClockIn(models.Model):
 	user = models.ForeignKey(User)
@@ -156,7 +150,7 @@ class ClockIn(models.Model):
 			
 		self.pay_rate = user.pay_rate
 		self.department = department
-		self.in_time = user.in_time = timezone.now()
+		self.in_time = user.in_time = datetime.now().replace(microsecond=0)
 		self.is_in = user.is_in = True
 		
 		print "self.in_time is..."
@@ -198,7 +192,7 @@ class ClockOut(models.Model):
 			
 		self.pay_rate = user.pay_rate
 		self.department = department
-		self.out_time = user.out_time = timezone.now()
+		self.out_time = user.out_time = datetime.now().replace(microsecond=0)
 		self.is_out = user.is_out = True
 		
 		print "self.out_time is.."
@@ -212,19 +206,40 @@ class ClockOut(models.Model):
 		
 		self.save()
 		return
-		
-	def hours(account, self):
-		td = self.out_time - self.in_time
-		return float(td.total_seconds()/3600)	
-		
-	def amount_payed(self):
-		return self.pay_rate * self.hours()
+
+class Timecard_Management(models.Model):
 	
-	def account_manage(amount_payed, account, self):
-		self.account = account
-		self.amount_payed = amount_payed
-		return self.amount_payed - self.account		
+	user = models.ForeignKey(User)
 	
+	def clockedin_hours(self, user, department):
+		total = user.out_time - user.in_time
+		print "out_time - in_time is %s" %(total)
+		
+		ten_hours = datetime.timedelta(hours=10)
+		
+		if total > ten_hours:
+			print "user clocked in over ten hours. forgot to clock out"
+			clock_out = ClockOut()
+			clock_out = clockOut(user, department)
+			return 	
+		
+		return
+		
+	def payment(self, user):
+		
+		self.account = user.account.total
+		self.payed = user.pay_rate * Timecard_Management.user.clockedin_hours(user, department)
+		
+		if user.accont.priority is 100 and user.account.total > 100:
+			subtract_account = self.payed - self.account
+			
+		elif user.account.total <= 100:
+			print "users account has less than $100. switch to next highest priority account"
+			#email someone or make some sort of warning!
+			# go to next account. store accounts in list of variable size???
+		
+		return
+		
 	def __unicode__(self):
 		return unicode(self.last_name) + ', ' + unicode(self.first_name) + ':' + unicode(self.number)
 
--- punchclock/views.py
+++ punchclock/views.py
@@ -9,12 +9,12 @@ from django.template import RequestContext
 def clockIn( request ):
 	if request.method == 'POST':
 		form = IndexForm( request.POST )
-		if form.is_valid( ):
+		if form.is_valid():
 			instance = form.save( commit=False )
 			user = User.objects.get( number=instance.student_number )
 			#name = User.objects.get( name=instance.last_name )
 			department = Department.objects.get( name=instance.department )
-			clock_in = ClockIn( )
+			clock_in = ClockIn()
 			clock_in.clockIn( user, department )	
 			
 		#	if clock_event.clock_in_out( user, department ) is False:
@@ -30,20 +30,20 @@ def clockIn( request ):
 		print 'Error! user didnt enter a field and hit clock in'
 		to_add = { }
 		to_add.update( csrf( request ) )
-		form = IndexForm( )
+		form = IndexForm()
 		to_add.update( { 'form': form } )
 		return render_to_response( 'punchclock/index.html', to_add )
 	
 def clockOut(request):
 	if request.method == 'POST':
 		form = IndexForm( request.POST )
-		if form.is_valid( ):
+		if form.is_valid():
 			instance = form.save( commit=False )
 			user = User.objects.get( number=instance.student_number )
 			department = Department.objects.get( name=instance.department )
-			clock_out = ClockOut( )
+			clock_out = ClockOut()
 			clock_out.clockOut( user, department )
-			return render_to_response( 'punchclock/clockout.html', {'first_name': user.first_name, 'last_name':user.last_name} )
+			return render_to_response( 'punchclock/clockout.html', {'first_name': user.first_name, 'last_name': user.last_name} )
 		else:
 			print 'Error! User didnt enter a field and hit clock out'
 			to_add = { }
